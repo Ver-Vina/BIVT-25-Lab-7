a644da4a30cfc7aa0dbb6c975f80a716e35b3616
Ver-Vina <vervina7@mail.ru>
2026-02-19 23:59:58 +0300

    Update Task1.cs

--- Lab7/Green/Task1.cs
+++ Lab7/Green/Task1.cs
@@ -4,5 +4,56 @@ namespace Lab7.Green
 {
     public class Task1
     {
+        public struct Participant
+        {
+            //поля
+            private string _surname;
+            private int _Group;
+            //private string _Trainer;
+            //private string _Result;
+            private int[] _Stars;
+            //свойства
+            public string Surname
+            {
+                get { return _surname; } // только для чтения
+            }
+            public int Group => _Group;
+            
+            public int[]Stars
+            {
+                get { }
+            }
+            public double Reiting
+            { 
+                get 
+                {
+                    if (_Stars==null|| _Stars.Length==0)
+                        return 0;
+                    double average = 0;
+                    for (int i = 0; i < _Stars.Length; i++)
+                    {
+                        average += _Stars[i];
+                    }
+                    return _Stars[0];
+                } 
+            }
+
+            //public double PassedTheStandard
+
+            //Конструктор
+            public Participant(string surname, int Group)
+            {
+                // проинициализировать ВСЕ поля
+                _surname = surname;
+                _Group = Group;
+                _Stars = new int[0];
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_surname);
+            }
+        }
     }
 }
+
